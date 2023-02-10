90ae800c742e5927664f6a4257c20beebc3bc3fd
Juan Carlos Diaz Lara <juancarlosdiazlara@gmail.com>
2023-02-10 03:58:07 +0000

    Add menu + intro

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,7 +22,51 @@ function getStory(name) {
                 }
 
             ]
-        }
+        },
+        howtoplay: {
+            title: "How to Play",
+            story: `Choose Your Own Adventure game typically involves making decisions and following a branching path through the story. Here are the steps to play: <br><br><br> Read the introduction: This sets the scene and introduces the protagonist, setting, and goal. <br><br> Make a decision: The game presents you with a choice, usually in the form of two or more options.<br>Turn to the designated page: The game will tell you which page to turn to based on the decision you made. <br><br> Read the outcome: The page you turn to will describe the outcome of your decision and present you with another choice. <br><br> Repeat steps 2-4: Keep making decisions and following the branching path until you reach the end of the story. <br><br> Check the ending: The ending will depend on the choices you made and the path you followed. Some endings may be good, some may be bad, and some may be a combination of both.`,
+            image: "howto.png",
+            choices: [{
+                choice: "Go home menu",
+                destination: 'menu'
+
+            }]
+        },
+        description: {
+            title: "Discover the Story",
+            story: `The Hero's Tale is an adventure game in which the player takes on the role of a hero on a quest to save the world from a great evil. <br> As the hero travels through a vast and varied world filled with magic, mystery, and legendary creatures, the game features an immersive storyline, challenging puzzles, and dangerous and intrepid battles. <br> In order to emerge as the ultimate champion and restore peace to the kingdom, the player must gather allies, acquire powerful weapons and spells, and uncover the truth behind the evil threat to the land.`,
+            image: "description.png",
+            choices: [{
+                choice: "Go home menu",
+                destination: 'menu'
+            }]
+        },
+        introduction: {
+            title: "Chapter 1 the misterious cave",
+            story: `The Hero's Tale is an adventure game in which the player takes on a challenge to find the legendary treasure guarded by an evil dragon, Spyron! In order to hold the highest order of royalty rank and prove to the people. <br> ${name} travels through the mountains and reaches the famous dungeon, known to be filled with mysterious creatures and challenging traps. If one is not careful, it’s game over.<br> If the ultimate champion finds the room, how is ${name} going to defeat the dragon? Is there a spell or a powerful weapon one must gather before? `,
+            image: "cave.png",
+            choices: [{
+                choice: "Let’s find out!",
+                destination: 'chapter1'
+            }, ]
+        },
+        chapter1: {
+            title: "Chapter 1: Zogar the Merciless",
+            story: `${name} You find yourself in a dark and dreary dungeon. Your quest begins, ${name}.<br> Remember you are a brave adventurer and it’s your only chance to prove to your people that you can be their leader.<br> Your search begins for the legendary treasure of the evil dragon, Zogar. <br> Now, you come across two paths, will you:`,
+            image: "caveinside.png",
+            choices: [{
+                    choice: "Take the left path through the damp and musty tunnel.",
+                    destination: 'leftpath'
+                },
+                {
+                    choice: "Take the right path through the torch-lit hallway.",
+                    destination: 'rightpath'
+                }
+            ]
+        },
+        
+
     }
 }
 document.addEventListener('DOMContentLoaded', function () {
